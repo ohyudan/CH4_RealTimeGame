eeeed8861c534231818a4a69804366cd0ca3db4d
ohyudan <ohyudan86@gmail.com>
2024-10-15 19:24:52 +0900

    강의까지

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { createServer } from 'http';
+import initSocket from './init/socket.js';
+import { loadGameAssets } from './init/assets.js';
+const app = express();
+const PORT = 4000;
+const server = createServer(app);
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+initSocket(server);
+
+app.use(express.static('public'));
+
+server.listen(PORT, async () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+
+  try {
+    const assets = await loadGameAssets();
+    console.log('Assets loaded successfully');
+  } catch (error) {
+    console.error('Failed to load game assets:', error);
+  }
+});
---
+++ src/handlers/helper.js
@@ -0,0 +1,39 @@
+import { createStage } from '../models/stage.model.js';
+import { getUsers, removeUser } from '../models/user.model.js';
+import { CLIENT_VERSION } from '../constants.js';
+import handlerMappings from './handlerMapping.js';
+export const handleDisconnect = (socket, uuid) => {
+  removeUser(socket.id);
+  console.log('User disconnected:' + socket.id);
+  console.log(`current users:`, getUsers());
+};
+
+export const handleConnection = (socket, userUUID) => {
+  console.log(`new user connected: ${userUUID} with socket ID ${socket.id}`);
+  console.log(`Current users:`, getUsers());
+
+  createStage(userUUID);
+
+  socket.emit('connection', { uuid: userUUID });
+};
+export const handleEvent = (io, socket, data) => {
+  if (!CLIENT_VERSION.includes(data.clientVersion)) {
+    socket.emit('response', {
+      status: 'fail',
+      message: 'Client version mismatch',
+    });
+    return;
+  }
+  const handler = handlerMappings[data.handlerId];
+  if (!handler) {
+    socket.emit('response', { status: 'fail', message: 'Handler not found' });
+    return;
+  }
+
+  const response = handler(data.userId, data.payload);
+  if (response.broadcast) {
+    io.emit('response', 'broadcast');
+    return;
+  }
+  socket.emit('response', response);
+};
